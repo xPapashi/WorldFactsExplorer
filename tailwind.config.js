8384e7626dd90191652e12f6ceba215a2e48c02c
Daniel <dtomkowski1@gmail.com>
2025-06-03 21:42:18 +0200

    Add tailwindcss to the project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./static/**/*.{html,js,jsx,ts,tsx}",
+    "./templates/**/*.{html,js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
